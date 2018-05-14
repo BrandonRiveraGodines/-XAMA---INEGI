1c4f92d6bc09fb4714f378046dd24c223a527e2a
BrandonRiveraGodines <brandonriveragodines@hotmail.com>
2018-05-14 15:51:10 -0500

    Commit_14052018
    El proyecto ya está avanzado, ya estan las dos primeras pantallas, dale

---
+++ XamarinInegi/XamarinInegi/XamarinInegi/Login.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinInegi
+{
+	public partial class Login : ContentPage
+	{
+		public Login()
+		{
+			InitializeComponent();
+            userLoginImage.Source = ImageSource.FromFile("user.png");
+		}
+    }
+}

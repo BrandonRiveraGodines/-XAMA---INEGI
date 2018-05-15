b960b47b4406f8e27c0b84f0f79e9143799cff05
BrandonRiveraGodines <brandonriveragodines@hotmail.com>
2018-05-15 15:30:59 -0500

    Commit_15052018_03
    Aun no se que hacer

--- XamarinInegi/XamarinInegi/XamarinInegi/Login.xaml.cs
+++ XamarinInegi/XamarinInegi/XamarinInegi/Login.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -16,18 +17,29 @@ namespace XamarinInegi
             //((NavigationPage.SetHasBackButton()))
             NavigationPage.SetHasBackButton(this, false);
             NavigationPage.SetHasNavigationBar(this, false);
+            this.BindingContext = this;
         }
 
         private void btnLinkToFunciones_Clicked(object sender, EventArgs e)
         {
             bool isEmailEmpty = string.IsNullOrEmpty(entEmailLogin.Text);
             bool isPassEmpty = string.IsNullOrEmpty(entPasswordLogin.Text);
+            string Email = entEmailLogin.Text.ToString();
+            string Password = entPasswordLogin.Text.ToString();
+            String URL_LOGIN = "http://192.168.0.4/laboratorio/Login.php";
 
             if (isEmailEmpty || isPassEmpty)
             {
                 
             } else
             {
+                using(HttpClient httpClient = new HttpClient())
+                {
+                    HttpRequestMessage httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, URL_LOGIN);
+                    
+                    
+                }
+
                 ((NavigationPage)this.Parent).PushAsync(new Funciones());
             }
         }
